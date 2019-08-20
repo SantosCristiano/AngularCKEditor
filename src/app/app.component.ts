b09efc864a00929e72769426220ea603e08ac1c1
Cristiano Santos <santosdesouzacris@gmail.com>
2019-08-20 16:08:02 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { CKEditorComponent } from 'ng2-ckeditor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularckeditor';
+  ckeditorContent: string= "<b>Probando contenido</b>";
+  @ViewChild(CKEditorComponent) ckEditor: CKEditorComponent;
+
+  ngAfterViewChecked() {
+    let editor = this.ckEditor.instance;
+    editor.config.height = '400';
+    editor.config.toolBarGroups = [
+      { name: 'document', groups: ['mode', 'document', 'doctools'] },
+      { name: 'clipboard', groups: ['clipboard', 'undo'] },
+      { name: 'editing', groups: ['find', 'selection', 'speelchecker', 'editing'] },
+      { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
+      { name: 'insert', groups: ['insert'] }
+    ]
+
+    editor.config.removeButtons = 'Source,Save,Templates,Find,Replace,Scayt,SelectAll,Form,Radio';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CKEditorModule } from 'ng2-ckeditor';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
